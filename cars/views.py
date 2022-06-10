fbd334c4551138b18013c0b91730da7031207dff
akast123 <a.kapoor@iitg.ac.in>
2022-06-10 18:03:01 +0530

    customised slider

--- cars/views.py
+++ cars/views.py
@@ -6,10 +6,12 @@ from .models import Car
 def index(request):
     new_cars = Car.objects.filter(category='New')[:6]
     used_cars = Car.objects.filter(category='Used')[:6]
+    latest_cars= Car.objects.all().order_by('-date')[:5]
 
     context={
         'new_cars':new_cars,
-        'used_cars':used_cars
+        'used_cars':used_cars,
+        'latest_cars':latest_cars
     }
     return render(request,'index.html', context)
 
